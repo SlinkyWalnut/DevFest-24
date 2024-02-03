ca37338432118ddaafa92b7e1bf0d3bc9973bcf8
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-02-03 17:19:52 -0500

    Fixed bug

--- AiSearch.py
+++ AiSearch.py
@@ -3,16 +3,17 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores.azuresearch import AzureSearch
 from langchain_community.document_loaders import AzureBlobStorageContainerLoader
 from langchain.text_splitter import CharacterTextSplitter
-from sqlalchemy.dialects.postgresql import UUID
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model: str = "text-embedding-ada-002"
 
-vector_store_address: str = f"https://${os.environ.get('AZURE_COGNITIVE_SEARCH_SERVICE_NAME')}.search.windows.net"
+print(os.environ.get('AZURE_COGNITIVE_SEARCH_SERVICE_NAME'))
 
+vector_store_address: str = f"https://{os.environ.get('AZURE_COGNITIVE_SEARCH_SERVICE_NAME')}.search.windows.net"
 
+print(vector_store_address)
 embeddings: OpenAIEmbeddings = OpenAIEmbeddings(deployment=model, chunk_size=1)
 index_name: str = "langchain-vector-demo"
 vector_store: AzureSearch = AzureSearch(
